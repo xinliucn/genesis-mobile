db83128294857120fc0554ab42a4ff3974389394
xinliucn <158404439+xinliucn@users.noreply.github.com>
2026-09-15 15:17:04 +0800

    feat: add world screen

---
+++ src/features/world/WorldScreen.tsx
@@ -0,0 +1,2 @@
+import { PlaceholderScreen } from '../../components/PlaceholderScreen';
+export function WorldScreen() { return <PlaceholderScreen title="世界" subtitle="你的行动正在创造这片世界" symbol="◇" />; }
